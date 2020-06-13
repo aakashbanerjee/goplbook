9090b6465c8f62297f02a787a84e1eb42cdebbc2
Aakash Banerjee <aakash.banerjee@gmail.com>
2020-06-12 22:42:55 -0700

    ch1 ex1.4

--- dup2/dup2.go
+++ dup2/dup2.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]map[string]int)
+	printed := make(map[string]bool)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
@@ -23,16 +24,23 @@ func main() {
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for name, linecount := range counts {
+		for line, n := range linecount {
+			if n > 1 {
+				if printed[name] == false {
+					fmt.Printf("Name of the file with duplicates: %v\n", name)
+					printed[name] = true
+				}
+				fmt.Printf("%v\t%v\n", n, line)
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]map[string]int) {
 	input := bufio.NewScanner(f)
+	counts[f.Name()] = make(map[string]int)
 	for input.Scan() {
-		counts[input.Text()]++
+		counts[f.Name()][input.Text()]++
 	}
 }
